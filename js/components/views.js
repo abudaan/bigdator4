019b3e825a8c7b8a279ae1974e324fc9845aa8ad
abudaan <daniel@abumarkub.net>
2017-07-05 16:13:55 +0200

    wip adding views

--- js/components/views.js
+++ js/components/views.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import R from 'ramda';
 
+const vega = global.vega; // coding like it's 1999
+
+const viewMap = {};
+
+const addView = (container, spec) => {
+    const elem: HTMLElement = document.createElement('div');
+    const name: string = spec.fileName.replace(/\./g, '_');
+    // console.log(container, spec);
+    elem.id = name;
+    elem.className = 'view';
+    container.appendChild(elem);
+    // const ds = R.clone(dataset);
+    viewMap[name] = new vega.View(vega.parse(spec))
+        .renderer('svg')
+        // .logLevel(vega.Debug)
+        .initialize(`#${name}`)
+        // .insert(dataset.name, dataset.values)
+        // .insert(ds.name, ds.values)
+        .run();
+};
+
 class Views extends Component {
     constructor() {
         super();
@@ -10,12 +31,13 @@ class Views extends Component {
 
     componentDidMount() {
         this.element = document.getElementById('left-pane');
-        console.log(this.element);
+        // console.log(this.element);
     }
 
     render() {
         if (R.isNil(this.element) === false) {
-            this.element.innerHTML = `bypass React ${this.index++}`;
+            R.forEach(spec => addView(this.element, spec), this.props.specs);
+            // addView(this.element, this.props.specs[0]);
         }
         return false;
     }
